5652439e5237f9b9b8f148fcdbf26a9431fc409e
Adam Michael <ajmichael0@gmail.com>
2014-07-09 03:47:17 -0400

    Added conflict detection with red backgrounds

--- grid.js
+++ grid.js
@@ -6,7 +6,6 @@ window.onload = function () {
     var xp = "//input[@maxlength='1']";
     var r = document.evaluate(xp,document,null,XPathResult.ORDERED_NODE_ITERATOR_TYPE,null);
     cells = new Array();
-    console.log(π(13,4));
     for (outrow = 0; outrow < 3; outrow++) {
         cells[outrow] = new Array();
         for (outcol = 0; outcol < 3; outcol++) {
@@ -16,6 +15,7 @@ window.onload = function () {
                 for (incol = 0; incol < 3; incol++) {
                     var cell = r.iterateNext();
                     cell.value = "";
+//                    cell.conflict = [false, false, false];  // Row, Col, Box
                     cells[outrow][outcol][inrow][incol] = cell;
                     count = count + 1;
                 }
@@ -30,14 +30,10 @@ function clearField(e,input) {
         e.preventDefault();
     } else if (keycode == 8 || keycode == 46 || (48 < keycode && keycode < 58)) {
         input.value = "";
+        setTimeout(function() {checkValid(input);});
     } else if (36 < keycode && keycode < 41) {
         var w,x,y,z;
-        for (var i = 0; i < 81; i++) {
-            w=π(i,27);x=π(i,9)%3;y=π(i,3)%3;z=i%3;
-            if (cells[w][x][y][z] == input) {
-                break;
-            }
-        }
+        [w,x,y,z] = getCoordinates(input);
         if (keycode == 37) {
             cells[w][ಠ_ಠ(x-!z,3)][y][ಠ_ಠ(z-1,3)].focus();
         } else if (keycode == 38) {
@@ -51,6 +47,85 @@ function clearField(e,input) {
         e.preventDefault();
     }
 }
+
+function checkValid(input) {
+    var coords = getCoordinates(input);
+    checkRow(coords);
+    checkCol(coords);
+    checkBox(coords);
+    //checkRowConflicts(coords);
+    //checkColConflicts(coords);
+    //checkBoxConflicts(coords);
+}
+
+function checkRow([w,x,y,z]) {
+    var val = cells[w][x][y][z].value;
+    if (val == "") return;
+    for (a = 0; a < 3; a++) {
+        for (b = 0; b < 3; b++) {
+            if ((a != x || b != z) && cells[w][a][y][b].value == val) {
+                for (x = 0; x < 3; x++) {
+                    for (z = 0; z < 3; z++) {
+                        cells[w][x][y][z].style.backgroundColor = "#CD9B9B";
+                       // cells[w][x][y][z].conflicts[0] = true;
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
+
+function checkCol([w,x,y,z]) {
+    var val = cells[w][x][y][z].value;
+    if (val == "") return;
+    for (a = 0; a < 3; a++) {
+        for (b = 0; b < 3; b++) {
+            if ((a != w || b != y) && cells[a][x][b][z].value == val) {
+                for (w = 0; w < 3; w++) {
+                    for (y = 0; y < 3; y++) {
+                        cells[w][x][y][z].style.backgroundColor = "#CD9B9B";
+                        //cells[w][x][y][z].conflicts[1] = true;
+                    }
+                }
+                return;
+            }
+        }
+    }
+}
+
+function checkBox([w,x,y,z]) {
+    var val = cells[w][x][y][z].value;
+    if (val == "") return;
+    for (a = 0; a < 3; a++) {
+        for (b = 0; b < 3; b++) {
+            if ((a != y || b != z) && cells[w][x][a][b].value == val) {
+                for (y = 0; y < 3; y++) {
+                    for (z = 0; z < 3; z++) {
+                        cells[w][x][y][z].style.backgroundColor = "#CD9B9B";
+                        //cells[w][x][y][z].conflicts[2] = true;
+                    }
+                }
+                return;
+            }
+        }
+    }
+
+
+}
+
+function getCoordinates(input) {
+    var w,x,y,z;
+    for (var i = 0; i < 81; i++) {
+        w=π(i,27);x=π(i,9)%3;y=π(i,3)%3;z=i%3;
+        if (cells[w][x][y][z] == input) {
+            break;
+        }
+    }
+    return [w,x,y,z];
+}
+
+funct
 
 function π(x,y) {
     return x/y|0;
